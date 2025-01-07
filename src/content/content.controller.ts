1ad581867718e1961684e1e7c51ef8a87222d0d6
Lautaro124 <lautaro.gonzalez@konecta-group.com>
2025-01-06 22:31:58 -0300

    Auth added

--- src/content/content.controller.ts
+++ src/content/content.controller.ts
@@ -1,16 +1,19 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { ContentService } from './content.service';
 import { ContentDto } from './dto/content.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('content')
 export class ContentController {
   constructor(private contentService: ContentService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   async create(@Body() content: ContentDto) {
     return await this.contentService.create(content);
   }
 
+  @UseGuards(AuthGuard)
   @Get(':module')
   async find(@Param('module') module: string) {
     return this.contentService.findModules(module);
